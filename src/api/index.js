e2ff43994f263b973288007541995e7991fda622
bluefin07 <bluefin07@163.com>
2022-02-25 23:28:20 +0800

    feat: 列表联调

--- src/api/index.js
+++ src/api/index.js
@@ -125,7 +125,7 @@ export default {
           url: '/leave/list',
           method: 'get',
           data: params,
-          mock: true
+          mock: false
       })
   },
   leaveOperate(params) {
