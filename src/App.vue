cf81a4f4c2f1e5f19425c47484d94186ec699c5b
bluefin07 <bluefin07@163.com>
2022-02-28 20:23:41 +0800

    feat: v-model demo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
   <router-view></router-view>
+  <!-- <v-input
+    v-model="userName"
+    v-model:title="title"
+    @handleSubmit="handleSubmit"
+  /> -->
 </template>
 
 <script>
+import { ref } from 'vue'
+import VInput from '@/components/VInput.vue'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    VInput
+  },
+  setup() {
+    let userName = ref('jack')
+    let title = ref('Vue3')
+    const handleSubmit = (value) => {
+      console.log('接受数据：', value)
+      console.log('当前对象：', userName)
+    }
+    return {
+      userName,
+      title,
+      handleSubmit
+    }
+  }
 }
 </script>
 
---
+++ src/components/VInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-input :modelValue="userName" @update:modelValue="handleInput"></el-input>
+  <el-button @click="handleSubmit">提交测试</el-button>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'VInput',
+  props: ['modelValue', 'title'],
+  setup(props, context) {
+    let userName = ref(props.modelValue)
+    const handleInput = (val) => {
+      userName.value = val
+      context.emit('update:modelValue', val)
+    }
+    const handleSubmit = () => {
+      context.emit('handleSubmit', userName)
+    }
+    return {
+      userName,
+      handleInput,
+      handleSubmit
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
